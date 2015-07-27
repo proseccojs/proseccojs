be1ef6aad9f0a92c367f7775d7d6bc049a13eef1
msmichellegar <msmichellegar@gmail.com>
2015-07-27 19:49:07 +0100

    changed styling and added navbar

--- api/server.js
+++ api/server.js
@@ -2,11 +2,11 @@ var Hapi = require('hapi'),
     server = new Hapi.Server();
 
 server.connection({
-  port: process.env.PORT
+  port: 8000
 });
 
 server.route(require('./routes.js'));
- 
+
 server.start(function() {
   console.log('Server running at: ' + server.info.uri + '!');
 });
