29b37cc60d0ab728f8da00c500ff7ef427679ebb
Claire Mitchell <claireinezmitchell@gmail.com>
2015-08-10 17:01:44 +0100

    Added testing stuff, still need to test pix handler

---
+++ test/serverTests.js
@@ -0,0 +1,13 @@
+var test = require("tape"),
+  server = require("../api/server.js"),
+  fs = require("fs");
+
+test("Check that index.html is sent when requested and returns 200 status code", function(t) {
+  server.inject({method: "GET", url: "/"}, function(response){
+    var page = fs.readFileSync(__dirname + "/../public/index.html", "utf-8");
+    t.equal(response.result, page, "Index.html is displayed as expected");
+    t.equal(response.statusCode, 200, "Status code is 200");
+    server.stop();
+    t.end();
+  });
+});
